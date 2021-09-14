356ad3717212bc3edd4a7dbd38b5d60b19401fba
JohnnyDevNull <philipp.john@live.de>
2021-09-14 19:53:14 +0200

    Upgrade to Angular 11

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import {LayoutModule} from "./core/layout/layout.module";
@@ -10,7 +10,7 @@ import {SearchService} from "./core/services/search.service";
 import {HttpModule} from "@angular/http";
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         LayoutModule,
@@ -29,7 +29,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
