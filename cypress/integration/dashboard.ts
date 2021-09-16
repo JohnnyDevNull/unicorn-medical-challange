8599dfb799c2e99533d221a426fd5397be03cf0f
JohnnyDevNull <philipp.john@live.de>
2021-09-16 16:18:34 +0200

    Add stack-list-item & weather-item e2e tests

--- cypress/integration/dashboard.ts
+++ cypress/integration/dashboard.ts
@@ -60,4 +60,39 @@ describe('root and dashboard page', () => {
     checkNavigation();
     checkDashboard();
   });
+
+  it('Should display a stack-item correctly', () => {
+    cy.visit('/dashboard');
+    waitForCalls();
+
+    cy.get('.stack-list.TypeScript .stack-list-item:first')
+      .contains('Views: 3')
+      .parent().contains('Answers: 0')
+      .parent().contains('Created: Sep 15, 2021')
+      .closest('.stack-list-item')
+      .contains('Socket.io + TypeScript: Socket.io not listening for new connections')
+      .closest('.stack-list-item').contains('(...read more)')
+      .closest('.stack-list-item').contains('node.js')
+      .closest('.stack-list-item').contains('typescript')
+      .closest('.stack-list-item').contains('express')
+      .closest('.stack-list-item').contains('sockets')
+      .closest('.stack-list-item').contains('socket.io');
+  });
+
+  it('Should display a weather-item correctly', () => {
+    cy.visit('/dashboard');
+    waitForCalls();
+
+    cy.get('.stack-list.Weather .weather-item:first')
+      .contains('Datum:')
+      .closest('.weather-item').contains('Zeit:')
+      .closest('.weather-item').contains('Temp. A.:')
+      .closest('.weather-item').contains('Temp. 3:')
+      .closest('.weather-item').contains('Feuchte A.:')
+      .closest('.weather-item').contains('Luftdruck:')
+      .closest('.weather-item').contains('Regen:')
+      .closest('.weather-item').contains('Wind:')
+      .closest('.weather-item').contains('Richtung:')
+      .closest('.weather-item').contains('Helligkeit:');
+  });
 });
